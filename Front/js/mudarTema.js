b573e9eafb221749ca1ca23d6d07b93155da123e
Kaue1025 <kauepazote.senai@gmail.com>
2024-08-29 11:15:25 -0300

    As imagens do menu linkadas certas

--- Front/js/mudarTema.js
+++ Front/js/mudarTema.js
@@ -5,18 +5,19 @@ icon.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 
     if (document.body.classList.contains("dark")) {
-        themeIcon.src = "./Front/src/sol.png";
-        document.getElementById("calendario-icon").src = "./Front/src/calenBranco.png"; 
-        document.getElementById("cardapio-icon").src = "./Front/src/cardapBranco.png"; 
+        themeIcon.src = "/Front/src/sol.png";
+        document.getElementById("calendario-icon").src = "/Front/src/calenBranco.png"; 
+        document.getElementById("cardapio-icon").src = "/Front/src/cardapBranco.png"; 
         document.querySelectorAll(".notificacao-icon").forEach(icon => {
-            icon.src = "./Front/src/sinoBranco.png"; 
+            icon.src = "/Front/src/sinoBranco.png"; 
         });
+
     } else {
-        themeIcon.src = "./Front/src/lua.png"; 
-        document.getElementById("calendario-icon").src = "./Front/src/calendario.png"; 
-        document.getElementById("cardapio-icon").src = "./Front/src/cardapio.png"; 
+        themeIcon.src = "/Front/src/lua.png"; 
+        document.getElementById("calendario-icon").src = "/Front/src/calendario.png"; 
+        document.getElementById("cardapio-icon").src = "/Front/src/cardapio.png"; 
         document.querySelectorAll(".notificacao-icon").forEach(icon => {
-        icon.src = "./Front/src/notificacao.png";
+        icon.src = "/Front/src/notificacao.png";
         });
     }
 });
