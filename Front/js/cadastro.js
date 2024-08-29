5708f13b3cddd0954013130ec9ce3e668b6051e8
DaviGeraldoDev <davi.geraldo.senai@gmail.com>
2024-08-29 15:16:28 -0300

    paginas ligadas e autenticacao funcionando +/-

--- Front/js/cadastro.js
+++ Front/js/cadastro.js
@@ -1,3 +1,21 @@
+var JWT = window.sessionStorage.getItem('JWT');
+	if (JWT == null || JWT == ''){
+		window.location.href = "login.html";
+	}
+var xhr = new XMLHttpRequest();
+var url = "http://localhost:5000/cliente";
+xhr.open("GET", url, true);
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.setRequestHeader("Token", window.sessionStorage.getItem('JWT'));
+xhr.onload = function () {
+		if (xhr.status !== 200) {
+            alert("Token expirado, faça login novamente!");
+			window.location.href = 'login.html';
+		}
+};
+xhr.responseType="text";
+xhr.send();    
+
 $(document).ready(function() {
     var url = "http://127.0.0.1:5000/cliente";
     var xhr = new XMLHttpRequest();
