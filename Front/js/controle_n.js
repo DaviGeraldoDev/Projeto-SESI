cfaa30588760d658dae5e8b47a3f007f5f817f2e
Kaue1025 <kauepazote.senai@gmail.com>
2024-09-03 15:51:20 -0300

    Arumando o tema dark

--- Front/js/controle_n.js
+++ Front/js/controle_n.js
@@ -71,6 +71,21 @@ cardapio.forEach(info_diaSemana => {
     render_cardapio(info_diaSemana)
 });
 
+//MODO DARK
+const icon = document.getElementById("MudarTema");
+const themeIcon = document.getElementById("MudarTema");
+
+icon.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        themeIcon.src = "/Front/src/sol.png";
+        
+    } else {
+        themeIcon.src = "/Front/src/lua.png";
+
+    }
+});
 
 
 
