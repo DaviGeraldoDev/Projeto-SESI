fe786bd85186780499358982c99beabde4671cea
DaviGeraldoDev <davi.geraldo.senai@gmail.com>
2024-09-10 13:46:49 -0300

    notificacoes certas

--- Front/js/Home_mariana.js
+++ Front/js/Home_mariana.js
@@ -1,3 +1,22 @@
+var JWT = window.sessionStorage.getItem('JWT');
+	if (JWT == null || JWT == ''){
+		window.location.href = "login.html";
+	}
+var xhr = new XMLHttpRequest();
+var url = "http://localhost:5000/cliente";
+xhr.open("GET", url, true);
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.setRequestHeader("Token", window.sessionStorage.getItem('JWT'));
+xhr.onload = function () {
+		if (xhr.status !== 200) {
+            alert("Token expirado, faça login novamente!");
+			window.location.href = 'login.html';
+		}
+};
+xhr.responseType="text";
+xhr.send();    
+
+
 // //MODO DARK
 const icon = document.getElementById("MudarTema");
 const themeIcon = document.getElementById("MudarTema");
