d232b779b424ebce8a4cd4458682244a2f33257a
DaviGeraldoDev <davi.geraldo.senai@gmail.com>
2024-10-03 15:00:06 -0300

    sistema adaptado para banco professor

--- Back/autenticacao/seg_senha.py
+++ Back/autenticacao/seg_senha.py
@@ -3,3 +3,7 @@ import hashlib
 def encrypt_md5(senha):
     senha_criptografada = hashlib.md5(senha.encode()).hexdigest()
     return senha_criptografada
+
+def encrypt_sha256(senha):
+    senha_criptografada = hashlib.sha256(senha.encode()).hexdigest()
+    return senha_criptografada
