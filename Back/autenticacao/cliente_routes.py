ea9298e7ae8f99d5a1774f4547a5dea5aba062b7
Vinicius0VT <vt36657492@gmail.com>
2024-11-12 09:55:46 -0300

    agenda ref

--- Back/autenticacao/cliente_routes.py
+++ Back/autenticacao/cliente_routes.py
@@ -1,11 +1,17 @@
 from flask_cors import CORS
 from flask import Blueprint, request, jsonify
-from cliente_service import cadastra_cardapio
+from cliente_service import cadastra_cardapio, AgendarRefeicao
 from db_functions import recuperar_imagem
 
 cliente_app = Blueprint('cliente_app',  __name__)
 
 CORS(cliente_app)
+
+@cliente_app.route('/refeicaoAgendada', methods=['POST'])
+def refeicaoAgendada():
+    data = request.get_json()
+    refeicaoAgendada = AgendarRefeicao(data)
+    return refeicaoAgendada
 
 @cliente_app.route('/obter-imagem', methods=['GET'])
 def obter_imagem():
