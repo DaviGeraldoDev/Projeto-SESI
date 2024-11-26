55ea9a2034e2fec95155d325373f6b69a08917d1
brunoaugustosenai <bruno.gimemezsenai@gmail.com>
2024-11-26 10:12:06 -0300

    select cafe da manha

--- DataBase(SQL)/logica_cafe_do_manha.sql
+++ DataBase(SQL)/logica_cafe_do_manha.sql
@@ -1,25 +1,28 @@
 select * from REFEICAO_AGENDADA
+select * from horarios
 
 ---cafe da manha !=0
-select cafe_manha, id_data,
+select cafe_manha, id_data, horario,
 count(*) as total
-from REFEICAO_AGENDADA
+from REFEICAO_AGENDADA, horarios
 where cafe_manha != '0'
 and id_data = '2024-11-12'
+and horario = '07:50'
 group by 
-cafe_manha, id_data
+cafe_manha, id_data, horario
 order by
-cafe_manha, id_data
+cafe_manha, id_data, horario
 
 ---cafe da manha != 1
-select cafe_manha, id_data,
+select cafe_manha, id_data, horario,
 count(*) as total
-from REFEICAO_AGENDADA
+from REFEICAO_AGENDADA, horarios
 where cafe_manha != '1'
 and id_data = '2024-11-12'
+and horario = '07:50'
 group by 
-cafe_manha, id_data
+cafe_manha, id_data, horario
 order by
-cafe_manha, id_data
+cafe_manha, id_data, horario
 
 
